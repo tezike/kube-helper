4bc6143c2692abab6000686c14d24be9daff984c
franko4don <franko4don@yahoo.com>
2020-02-21 12:39:02 +0100

    added rabbitmq config

--- init.sh
+++ init.sh
@@ -10,6 +10,7 @@ kubectl apply -f grafana/config.yml
 
 helm install stable/grafana -f grafana/values.yml --namespace monitoring --name grafana
 
+# generate wildcard ssl secret
 kubectl create secret tls wildcard-certificate-tls -n dev --key="privkey.pem" --cert="fullchain.pem"
 
 # Exposes grana to external source 
@@ -19,3 +20,10 @@ kubectl get secret \
     --namespace monitoring grafana \
     -o jsonpath="{.data.admin-password}" \
     | base64 --decode ; echo
+
+# Install Rabbitmq
+
+helm install --name xend-broker-svc --namespace production --set rabbitmqUsername=admin \
+--set rabbitmqErlangCookie=secretcookie \
+--set rabbitmqErlangCookie=secretcookie --set rbacEnabled=true --set service.type=NodePort --set ingress.enabled=true \
+--set ingress.hostName="devbroker.xen.c" --set ingress.tls=true --set ingress.tlsSecret="wildcard-certificate-tls" stable/rabbitmq-ha
