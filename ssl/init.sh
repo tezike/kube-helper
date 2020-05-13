3fbfbe4bb12247494929339a2864c8946ce28ba4
franko4don <franko4don@yahoo.com>
2020-05-13 17:07:24 +0100

    added secret creater

--- ssl/init.sh
+++ ssl/init.sh
@@ -1,1 +1,4 @@
-sudo certbot certonly --manual -d *.example.com -d example.com --agree-tos --no-bootstrap --manual-public-ip-logging-ok --preferred-challenges dns-01 --server https://acme-v02.api.letsencrypt.org/directory
+sudo certbot certonly --manual -d *.xend.tk -d xend.tk --agree-tos --no-bootstrap --manual-public-ip-logging-ok --preferred-challenges dns-01 --server https://acme-v02.api.letsencrypt.org/directory
+
+
+kubectl create secret tls -n production wildcard-certificate-tls --cert fullchain.pem --key privkey.pem
