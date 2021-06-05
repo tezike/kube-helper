5835f8c08c1a43a51660b1f8f0d448344322d1fb
Ezike Tochukwu <31063326+tezike@users.noreply.github.com>
2021-06-05 14:19:40 +0100

    add notes for tiller

--- ingress-controller/init.sh
+++ ingress-controller/init.sh
@@ -2,6 +2,8 @@ helm repo add nginx-stable https://helm.nginx.com/stable
 helm repo update
 helm install stable/nginx-ingress --name nginx-ingress --set controller.publishService.enabled=true
 
+
+#tiller here is used for as a service account for clusterwide-access reference https://v2.helm.sh/docs/using_helm/#tiller-and-role-based-access-control
 helm init --upgrade --service-account tiller
 
 # kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.11/deploy/manifests/00-crds.yaml
